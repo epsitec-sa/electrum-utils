79ade3759b1846d923704739e39ece54dc1884f1
Pierre Arnaud <arnaud@epsitec.ch>
2016-02-05 13:25:57 +0100

    Add positive int parse/check.

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@
 module.exports = {
   getInstanceMethodNames: require ('./get-instance-method-names.js'),
   getTextSelection: require ('./get-text-selection.js'),
+  isPositiveInt: require ('./parse-positive-int.js').isPositiveInt,
+  parsePositiveInt: require ('./parse-positive-int.js').parsePositiveInt,
   shallowEqual: require ('./shallow-equal.js'),
   startsWithUpperCase: require ('./starts-with-upper-case.js'),
-  startsWithLowerCase: require ('./starts-with-lower-case.js'),
+  startsWithLowerCase: require ('./starts-with-lower-case.js')
 };
 
 /******************************************************************************/
